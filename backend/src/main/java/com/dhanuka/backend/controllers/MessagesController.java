8aa2bb7a3ca074fca6c93326668dc589b6d4f4eb
Peheliya Dhanuka Navod <140178910+peheliyanavod@users.noreply.github.com>
2026-08-16 16:25:50 +0530

    setup project backend

---
+++ backend/src/main/java/com/dhanuka/backend/controllers/MessagesController.java
@@ -0,0 +1,22 @@
+package com.dhanuka.backend.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class MessagesController {
+    @GetMapping("/messages")
+    public ResponseEntity<List<String>> messages(){
+        return ResponseEntity.ok(Arrays.asList("first", "second"));
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return "Testing Successful!";
+    }
+
+}
